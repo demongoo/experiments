fa46f2e7471fea11ba2b0561873831cb02d955c5
Dmitry Revenko <dmitry.revenko@gmail.com>
2012-10-16 03:18:02 +0400

    Fast fibo generationusing Binet formula

--- fibs/fibs.scala
+++ fibs/fibs.scala
@@ -1,5 +1,7 @@
 package goo.math
 
+import scala.math._
+
 object Fibonacci {
   /**
    * Recursive fibonacci generation
@@ -9,7 +11,7 @@ object Fibonacci {
    * @param n Number
    * @return nth fibonacci number
    */
-  def recursive(n: Int): Int = n match {
+  def recursive(n: Int): Long = n match {
     case 0 => 0
     case 1 => 1
     case n => recursive(n - 1) + recursive(n - 2)
@@ -18,18 +20,19 @@ object Fibonacci {
   /**
    * Looping fibonacci generation
    * 
-   * Polinomial time O(f^n) ?
+   * Time O(n) ? should be faster
+   * than recursive and spares stack
    * 
    * @param n Number
    * @return nth fibonacci number
    */
-  def loop(n: Int): Int = n match {
+  def loop(n: Int): Long = n match {
     case 0 => 0
     case 1 => 1
     case n => {
-      var (f1, f2, i) = (0, 1, 2)
+      var f1: Long = 0; var f2: Long = 1; var i = 2
       while (i <= n) {
-        val f = f1 + f2
+        val f: Long = f1 + f2
         f1 = f2
         f2 = f
         i += 1
@@ -37,4 +40,29 @@ object Fibonacci {
       f2
     }
   }
+  
+  
+  lazy val sqrt5 = sqrt(5)
+  lazy val phi: Double = (1 + sqrt5) / 2
+  lazy val psi: Double = (1 - sqrt5) / 2
+  
+  /**
+   * Rounding fibonacci generation
+   * 
+   * O(1)
+   * 
+   * @param n Number
+   * @return nth fibonacci number
+   */
+  def rounding(n: Int): Long = round(pow(phi, n) / sqrt5).toLong
+  
+  /**
+   * Binet fibonacci generation
+   * 
+   * O(1)
+   * 
+   * @param n Number
+   * @return nth fibonacci number
+   */
+  def binet(n: Int): Long = ((pow(phi, n) - pow(psi, n)) / sqrt5).toLong
 }
