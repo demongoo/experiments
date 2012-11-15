a5f6270edfcb5b7765d12d361ab807c153a671a0
Dmitry Revenko <dmitry.revenko@gmail.com>
2012-11-16 01:54:15 +0400

    p05.rb

--- 99/ruby/p05.rb
+++ 99/ruby/p05.rb
@@ -4,13 +4,22 @@ require_relative "lists.rb"
 
 # will just monkey patch list class
 class List
-  # by going to end and appending
+  # by going to end and constructing in reverse order
   def self.reverse(ls)
     reversed = Nil.instance
     ls.each do |el|
       reversed = reversed << el
     end
     reversed
+  end
+
+  # in somewhat recursive manner (worse that first)
+  def self.reverse2(ls)
+    if ls.empty?
+      ls
+    else
+      self.reverse2(ls.tail) >> ls.head
+    end
   end
 end
 
@@ -21,5 +30,6 @@ class Tests
     
     puts ls
     puts List.reverse ls
+    puts List.reverse2 ls
   end
 end
